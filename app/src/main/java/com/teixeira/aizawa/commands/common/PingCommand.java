7f9b0c01d411ca6a3ecc6936c890afe1ff0b197e
teixeira0x <secretedit123e@gmail.com>
2024-04-22 14:46:59 -0300

    send ping message with an embed

--- app/src/main/java/com/teixeira/aizawa/commands/common/PingCommand.java
+++ app/src/main/java/com/teixeira/aizawa/commands/common/PingCommand.java
@@ -1,6 +1,7 @@
 package com.teixeira.aizawa.commands.common;
 
 import com.teixeira.aizawa.core.command.SlashCommand;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 public class PingCommand extends SlashCommand {
@@ -11,6 +12,9 @@ public class PingCommand extends SlashCommand {
   @Override
   public void execute(SlashCommandInteractionEvent event) {
     long ping = event.getJDA().getGatewayPing();
-    event.reply("Meu ping atual: " + ping).queue();
+
+    EmbedBuilder embed = new EmbedBuilder();
+    embed.setDescription("Meu ping atual: " + ping);
+    event.replyEmbeds(embed.build()).queue();
   }
 }
